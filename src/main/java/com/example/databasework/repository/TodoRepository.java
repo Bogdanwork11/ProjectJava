f047bf022b70b439df96fc3ae8204d11eac9ad19
Bogdan <Bogdankosyanenko@icloud.com>
2026-07-13 17:48:53 +0500

    Added method findByAuthorId for HIBERNATE

--- src/main/java/com/example/databasework/repository/TodoRepository.java
+++ src/main/java/com/example/databasework/repository/TodoRepository.java
@@ -4,6 +4,15 @@ import com.example.databasework.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+
+    List<TodoEntity> findByAuthorId(Integer authorId);
+
+    //Sql под капотом
+    //select * from todo_entity
+    //where author_id = ?
+
 }
