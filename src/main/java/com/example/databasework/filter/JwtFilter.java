4e21df51d232272e1a0e0405a9985bea17aaa222
Bogdan <Bogdankosyanenko@icloud.com>
2026-07-16 15:03:43 +0500

    changed structure filter for token linked with controllers

--- src/main/java/com/example/databasework/filter/JwtFilter.java
+++ src/main/java/com/example/databasework/filter/JwtFilter.java
@@ -1,26 +1,27 @@
-
 package com.example.databasework.filter;
 
+import com.example.databasework.Role;
 import com.example.databasework.entity.UserEntity;
 import com.example.databasework.repository.UserRepository;
 import com.example.databasework.service.JWTService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
 @Component
-public class JwtFilter {
+public class JwtFilter { //todo to read about springboot filters
     private final JWTService jWTService;
     private final UserRepository userRepository;
 
-    private String secret = "mySuperSecretKeyThatIsAtLeast32CharactersLong!!!!";
+    @Value("${spring.jwt.secret}")
+    private String secret;
 
     public JwtFilter(JWTService jWTService, UserRepository userRepository) {
         this.jWTService = jWTService;
         this.userRepository = userRepository;
-
     }
 
     public Claims extractClaims(String token) {
@@ -31,20 +32,18 @@ public class JwtFilter {
 
         String login = claims.getSubject();
         String role = claims.get("role", String.class);
-        System.out.println(role);
 
         return claims;
 
     }
+
     public void validateActiveUser(String token) {
 
         Claims claims = extractClaims(token);
 
         String login = claims.getSubject();
-        System.out.println("Login из токена = " + login);
 
         UserEntity user = userRepository.findByLogin(login);
-        System.out.println("User = " + user);
 
         if (user != null) {
             System.out.println("DB login = " + user.getLogin());
@@ -58,6 +57,19 @@ public class JwtFilter {
         if (!Boolean.TRUE.equals(user.getIsActive())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is inactive");
         }
+
+    }
+
+    public Role authentificate(String authHeader) {
+        String token = authHeader.replace("Bearer", "");
+        validateActiveUser(token);
+        return Rolecheck(token);
+    }
+
+    public Role Rolecheck(String token) {
+        Claims claims = extractClaims(token);
+        String roleStr = claims.get("role", String.class);
+        return Role.fromString(roleStr);
     }
 }
 
