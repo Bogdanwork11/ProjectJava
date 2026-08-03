4f206b8805bf145dc953a7b7580731f3613cf4df
Bogdan <Bogdankosyanenko@icloud.com>
2026-08-03 12:33:51 +0500

    changed linked with finduser

--- src/main/java/com/example/databasework/repository/UserRepository.java
+++ src/main/java/com/example/databasework/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.databasework.repository;
 
-import com.example.databasework.entity.User;
+import com.example.databasework.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-    public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByLogin(String login);
+    public interface UserRepository extends JpaRepository<Users, Integer> {
+    Users findByLogin(String login);
     }
 
