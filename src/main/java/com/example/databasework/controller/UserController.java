6f208b6cb4fbd007f7bec1c2e61a6d569a6576a8
Bogdan <Bogdankosyanenko@icloud.com>
2026-08-11 10:00:19 +0500

    deleted not unnecessary imports

--- src/main/java/com/example/databasework/controller/UserController.java
+++ src/main/java/com/example/databasework/controller/UserController.java
@@ -7,9 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
